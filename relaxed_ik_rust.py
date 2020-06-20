94478c2822a43df8b6171e62913032a83b65615b
Hashiyaa <haochen213300@gmail.com>
2020-06-20 18:32:37 -0500

    Fix some bugs

--- relaxed_ik_rust.py
+++ relaxed_ik_rust.py
@@ -9,16 +9,25 @@ from std_msgs.msg import Float64
 # from RelaxedIK.Utils.colors import bcolors
 # from RelaxedIK.relaxedIK import get_relaxedIK_from_info_file
 from ament_index_python.packages import get_package_share_directory
-from cffi import FFI
-
 # may raise PackageNotFoundError
-package_share_directory = get_package_share_directory('relaxed_ik_ros2')
+import ctypes
 
-ffi = FFI()
-ffi.cdef("""
-    double* run(double **pos_goals, double **quat_goals);
-""")
-C = ffi.dlopen(package_share_directory + '/relaxed_ik_core/target/debug/librelaxed_ik_node_ros2.so')
+class Opt(ctypes.Structure):
+    _fields_ = [("data", ctypes.POINTER(ctypes.c_double)), ("length", ctypes.c_int)]
+
+
+package_share_directory = get_package_share_directory('relaxed_ik_ros2')
+os.chdir(package_share_directory + '/relaxed_ik_core')
+
+lib = ctypes.cdll.LoadLibrary(package_share_directory + '/relaxed_ik_core/target/debug/librelaxed_ik_ros2.so')
+lib.rust_run.restype = Opt
+
+# from cffi import FFI
+# ffi = FFI()
+# ffi.cdef("""
+#     double* run(double **pos_goals, double **quat_goals);
+# """)
+# C = ffi.dlopen(package_share_directory + '/relaxed_ik_core/target/debug/librelaxed_ik_ros2.so')
 
 eepg = None
 def eePoseGoals_cb(msg):
@@ -28,7 +37,7 @@ def eePoseGoals_cb(msg):
 def main(args=None):
     global eepg
 
-    print("\nSolver initialized!\n")
+    print("\nSolver initialized!\n", flush=True)
 
     rclpy.init()
     node = rclpy.create_node('relaxed_ik')
@@ -38,35 +47,39 @@ def main(args=None):
 
     rclpy.spin_once(node)
 
+    print("\nHeard!\n", flush=True)
+
     rate = node.create_rate(3000.0)
 
     while rclpy.ok():
-        pos_goals = []
-        quat_goals = []
         pose_goals = eepg.ee_poses
         header = eepg.header
+        pos_arr = (ctypes.c_double * (3 * len(pose_goals)))()
+        quat_arr = (ctypes.c_double * (4 * len(pose_goals)))()
 
-        for p in pose_goals:
-            pos_x = p.position.x
-            pos_y = p.position.y
-            pos_z = p.position.z
+        # print(type(pos_arr), flush=True)
 
-            quat_w = p.orientation.w
-            quat_x = p.orientation.x
-            quat_y = p.orientation.y
-            quat_z = p.orientation.z
+        for i in range(len(pose_goals)):
+            p = pose_goals[i]
+            pos_arr[3*i] = p.position.x
+            pos_arr[3*i+1] = p.position.y
+            pos_arr[3*i+2] = p.position.z
 
-            pos_goals.append([pos_x, pos_y, pos_z])
-            quat_goals.append([quat_w, quat_x, quat_y, quat_z])
+            quat_arr[3*i] = p.orientation.w
+            quat_arr[3*i+1] = p.orientation.x
+            quat_arr[3*i+2] = p.orientation.y
+            quat_arr[3*i+3] = p.orientation.z
 
-        xopt = C.run(pos_goals, quat_goals)
+        xopt = lib.rust_run(pos_arr, len(pos_arr), quat_arr, len(quat_arr))
+
+        # print(xopt.data[0], flush=True)
+
         ja = JointAngles()
         ja.header = header
-        for x in xopt:
-            ja.angles.data.append(x)
+        for i in range(xopt.length):
+            ja.angles.data.append(xopt.data[i])
 
         angles_pub.publish(ja)
-        # print xopt
 
         rate.sleep()
 
